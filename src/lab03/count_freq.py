41d60401c5bddce5d618da2fa3e7c3e8a231a813
darinaby2008 <darinaby2008@mail.ru>
2025-10-14 02:37:10 +0300

    -

---
+++ src/lab03/count_freq.py
@@ -0,0 +1,10 @@
+import re
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    unique_words = list(set(tokens))
+    list_count = [tokens.count(i) for i in unique_words]
+    dict_count = {key: word for key, word in list(zip(unique_words, list_count))}
+    return dict_count
+
+print(count_freq(["a","b","a","c","b","a"]))
+print(count_freq(["bb","aa","bb","aa","cc"]))
