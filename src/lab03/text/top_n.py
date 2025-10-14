640930288bfeb18c47d7519b4153c68a556c020b
darinaby2008 <darinaby2008@mail.ru>
2025-10-14 03:14:41 +0300

    -

---
+++ src/lab03/text/top_n.py
@@ -0,0 +1,16 @@
+import re
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    unique_words = list(set(tokens))
+    list_count = [tokens.count(i) for i in unique_words]
+    dict_count = {key: word for key, word in list(zip(unique_words, list_count))}
+    return dict_count
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    list_dict = list(freq.items())
+    top = sorted(list_dict, key=lambda x:  x[0])
+    top_plus = sorted(top, key=lambda x: x[1], reverse=True)[:n]
+    return top_plus
+
+print(top_n(count_freq(["a","b","a","c","b","a"]),n=2))
+print(top_n(count_freq(["bb","aa","bb","aa","cc"]),n=2))
