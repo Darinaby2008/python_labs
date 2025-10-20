595275c85c82e41b2c2cd1dd6523cb0ee9ef2885
darinaby2008 <darinaby2008@mail.ru>
2025-10-20 19:20:16 +0300

    -

---
+++ src/_lib_/lib-text.py
@@ -0,0 +1,28 @@
+import re
+
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    if casefold:
+         text = text.casefold()
+    if yo2e:
+        text = text.replace('ё','е').replace('Ё','Е') 
+    text = text.strip()
+    text = re.sub(r'[\t\r\n]', ' ', text) 
+    text = ' '.join(text.split())
+    return text
+
+def tokenize(text: str) -> list[str]:
+    pattern = r'\w+(?:-\w+)*'
+    tokens  = re.findall(pattern, text)
+    return tokens
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    unique_words = list(set(tokens))
+    list_count = [tokens.count(i) for i in unique_words]
+    dict_count = {key: word for key, word in list(zip(unique_words, list_count))}
+    return dict_count
+
+def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
+    list_dict = list(freq.items())
+    top = sorted(list_dict, key=lambda x:  x[0])
+    top_plus = sorted(top, key=lambda x: x[1], reverse=True)[:n]
+    return top_plus
