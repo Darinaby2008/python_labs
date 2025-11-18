51f506a3e7cb1da9fdf60d2f811220b44c65381f
darinaby2008 <darinaby2008@mail.ru>
2025-11-18 10:39:27 +0300

    Update cli_convert.py

--- src/lab06/cli_convert.py
+++ src/lab06/cli_convert.py
@@ -1,24 +1,83 @@
 import sys
+import argparse
+
+from lib import csv_to_xlsx
+from lib import json_to_csv
+from lib import csv_to_json
+from cli_text import check_file
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Конвертеры данных")
+    sub = parser.add_subparsers(dest="command", required=True) 
+    
+    p1 = sub.add_parser("json2csv")
+    p1.add_argument("--in", dest="input", required=True, help="Входной JSON файл")
+    p1.add_argument("--out", dest="output", required=True, help="Выходной CSV файл")
+
+    p2 = sub.add_parser("csv2json")
+    p2.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
+    p2.add_argument("--out", dest="output", required=True, help="Выходной JSON файл")
+
+    p3 = sub.add_parser("csv2xlsx")
+    p3.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
+    p3.add_argument("--out", dest="output", required=True, help="Выходной XLSX файл")
+    
+    args = parser.parse_args()
+
+    
+    if args.command == "json2csv":
+        if not check_file(args.input):
+            print(f"Ошибка: Файл {args.input} не существует или недоступен")
+            sys.exit(1)
+                
+        json_to_csv(args.input, args.output)
+        print(f"Успешно: JSON -> CSV")
+            
+    elif args.command == "csv2json":
+        if not check_file(args.input):
+            print(f"Ошибка: Файл {args.input} не существует или недоступен")
+            sys.exit(1)
+                
+        csv_to_json(args.input, args.output)
+        print(f"Успешно: CSV -> JSON")
+            
+    elif args.command == "csv2xlsx":
+        if not check_file(args.input):
+            print(f"Ошибка: Файл {args.input} не существует или недоступен")
+            sys.exit(1)
+                
+        csv_to_xlsx(args.input, args.output)
+        print(f"Успешно: CSV -> XLSX")
+            
+    else:
+        print("Ошибка: Неизвестная команда")
+        sys.exit(1)
+    return 0
+        
+
+if __name__ == "__main__":
+    main()
+
+
+'''import sys
 import os
 import argparse
 import json
 import csv
 
 def check_file(file_path: str) -> bool:
-    """Проверяет что файл существует"""
     if not os.path.isfile(file_path):
         print(f"Файл не найден: {file_path}", file=sys.stderr)
         return False
     return True
 
 def ensure_dir(file_path: str):
-    """Создает папку для выходного файла если нужно"""
     dir_name = os.path.dirname(file_path)
     if dir_name and not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
 def json2csv(input_file: str, output_file: str):
-    """Конвертирует JSON в CSV"""
     if not check_file(input_file):
         sys.exit(1)
     
@@ -133,7 +192,7 @@ def main():
         csv2xlsx(args.input, args.output)
 
 if __name__ == "__main__":
-    main()
+    main()'''
 
 
 '''import sys, argparse
