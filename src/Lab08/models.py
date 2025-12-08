343235e76acf41e5874036ab3f50ac0a7cd519ba
darinaby2008 <darinaby2008@mail.ru>
2025-12-08 20:24:41 +0300

    -

--- src/Lab08/models.py
+++ src/Lab08/models.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime, date
-from typing import Dict, Any
+
 
 @dataclass
 class Student:
@@ -13,41 +13,39 @@ class Student:
         try:
             datetime.strptime(self.birthdate, "%Y-%m-%d")
         except ValueError:
-            raise ValueError(f"Invalid date format: {self.birthdate}. Use YYYY-MM-DD")
+            raise ValueError("warning: birthdate format might be invalid")
         
         if not (0 <= self.gpa <= 5):
-            raise ValueError(f"GPA must be between 0 and 5, got {self.gpa}")
+            raise ValueError("gpa must be between 0 and 5")
+
 
     def age(self) -> int:
-        birth_date = datetime.strptime(self.birthdate, "%Y-%m-%d").date()
+        bdate = datetime.strptime(self.birthdate, "%Y-%m-%d").date()
         today = date.today()
-        age = today.year - birth_date.year
-        
-        if today.month < birth_date.month or (today.month == birth_date.month and today.day < birth_date.day):
-            age -= 1
-            
-        return age
+        return today.year - b.year - ((today.month, today.day) < (bdate.month, bdate.day))
 
-    def to_dict(self) -> Dict[str, Any]:
+    def to_dict(self) -> dict:
         return {
             "fio": self.fio,
             "birthdate": self.birthdate,
+            "gpa": self.gpa,
             "group": self.group,
-            "gpa": self.gpa
         }
 
     @classmethod
-    def from_dict(cls, data: Dict[str, Any]) -> "Student":
+    def from_dict(cls, d: dict):
         return cls(
-            fio=data["fio"],
-            birthdate=data["birthdate"],
-            group=data["group"],
-            gpa=data["gpa"]
-        )
+            fio=d["fio"],
+            birthdate=d["birthdate"],
+            group=d["group"],
+            gpa=d["gpa"],
+    )
 
-    def __str__(self) -> str:
-        return f"Студент: {self.fio}, Группа: {self.group}, GPA: {self.gpa}, Возраст: {self.age()} лет"
 
+
+    def __str__(self):
+        return f"Student {self.fio},from {self.group},have {self.gpa}"
+            
 if __name__ == "__main__":
     try:
         student = Student(
@@ -58,5 +56,5 @@ if __name__ == "__main__":
         )
         print(student)
         print(f"Словарь: {student.to_dict()}")
-    except ValueError as e:
-        print(f"Ошибка: {e}")
+    except ValueError:
+        raise ValueError("Ошибка")
